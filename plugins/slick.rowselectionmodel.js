dd10973e2337a540de052be165f413593b5e0171
Simon Jesenko <simon.jesenko@gmail.com>
2018-06-28 16:29:23 +0200

    Prevent shift - up / down selection of non-multiselect grids

--- plugins/slick.rowselectionmodel.js
+++ plugins/slick.rowselectionmodel.js
@@ -99,7 +99,7 @@
 
     function handleKeyDown(e) {
       var activeRow = _grid.getActiveCell();
-      if (activeRow && e.shiftKey && !e.ctrlKey && !e.altKey && !e.metaKey && (e.which == 38 || e.which == 40)) {
+      if (activeRow && _grid.getOptions().multiSelect && e.shiftKey && !e.ctrlKey && !e.altKey && !e.metaKey && (e.which == 38 || e.which == 40)) {
         var selectedRows = getSelectedRows();
         selectedRows.sort(function (x, y) {
           return x - y
